35bccd265fbbd24e0126e7f4f61cc75bc39c2259
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-10-18 12:55:55 +0300

    add bloor

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const burger = document.querySelector('.burger')
 const body = document.querySelector('body')
+const bloor = document.querySelector('.bloor')
 const nav = document.querySelector('.nav')
+const ul = document.querySelector('.nav__links')
+
 
 nav.addEventListener('click', (e) => {
     if ((e.target.localName == 'a') && (burger.classList.contains('burger-rot'))) {
         closeMenu()
     }
 })
+bloor.addEventListener('click', closeMenu)
+
 function closeMenu() {
-    nav.classList.remove('show-nav')
-    burger.classList.remove('burger-rot')
+  body.classList.remove('hidden')
+  bloor.classList.remove('show')
+  nav.classList.remove('show-nav')
+  ul.classList.remove('show-ul')
+  burger.classList.remove('burger-rot')
 }
 burger.addEventListener('click', () => {
-    nav.classList.toggle('show-nav')
-    burger.classList.toggle('burger-rot')
+  body.classList.toggle('hidden')
+  bloor.classList.toggle('show')
+  nav.classList.toggle('show-nav')
+  ul.classList.toggle('show-ul')
+  burger.classList.toggle('burger-rot')
 })
